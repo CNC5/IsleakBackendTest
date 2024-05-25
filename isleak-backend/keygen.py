7f1f21397c3472b918c528946b4f486d3a2e04b8
CNC5 <kokfbbat@mail.ru>
2024-05-26 00:43:04 +0300

    Tweaks: keygen adding new keys by default

--- isleak-backend/keygen.py
+++ isleak-backend/keygen.py
@@ -1,8 +1,13 @@
 from database import OperatorAliases, APIKey
 import secrets
 
-def create_new_key():
+
+def add_new_key():
     database = OperatorAliases()
     new_key = secrets.token_urlsafe(64)
     database.insert_rows([APIKey(api_key=new_key)])
     return new_key
+
+
+if __name__ == '__main__':
+    print(add_new_key())
